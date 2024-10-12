d835fc27900ebeed36f8114001b36168262cd477
syed bilal haider <bilaldev151214@gmail.com>
2024-10-12 19:45:39 +0200

    Custom command and beforeEach

--- cypress/e2e/fundamental.cy.js
+++ cypress/e2e/fundamental.cy.js
@@ -1,12 +1,13 @@
 describe('template spec', () => {
+
+  beforeEach(()=>{
+    cy.visit('http://192.168.0.101:5173/')
+  })
   it('Contains correct header text', () => {
-    cy.visit('http://192.168.0.101:5174/')
-    cy.get('[data-test="expense-header"]').should('contain.text', "Expense Tracker")
+    cy.getData("expense-header").should('contain.text', "Expense Tracker")
   })
 
   it('Accordion works correctly', () => {
-    cy.visit('http://192.168.0.101:5174/')
-    
     cy.contains(/Text/i).should('not.be.visible')
 
     cy.get('[data-test="accordian-item-two"] div.accordion-header').click()
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+ Cypress.Commands.add('getData', (getSelectedData) => {
+    return cy.get(`[data-test=${getSelectedData}]`)
+ })
